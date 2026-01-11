658d609d802d5969d4926afa5f8e0ef933916f71
Sushantmg <sushantmg2004@gmail.com>
2026-01-11 18:17:08 +0545

    mailService

--- utils/mailService.ts
+++ utils/mailService.ts
@@ -1,13 +1,37 @@
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: 587,
-  secure: false, // Use true for port 465, false for port 587
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass:  process.env.EMAIL_PASSWORD,
-  },
-});
+let transporter: nodemailer.Transporter | null = null;
 
-export {transporter};
+export async function initMailService() {
+  if (process.env.NODE_ENV === "development") {
+    const testAccount = await nodemailer.createTestAccount();
+
+    transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        user: testAccount.user,
+        pass: testAccount.pass,
+      },
+    });
+
+    console.log("📧 Ethereal email:", testAccount.user);
+  } else {
+    transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+}
+
+export function getTransporter() {
+  if (!transporter) {
+    throw new Error("Mail transporter not initialized");
+  }
+  return transporter;
+}
